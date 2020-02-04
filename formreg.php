23209fe64c3564526e590e0e8d2599ca8aec9cb2
FjavierMunozSanchez <43465391+FjavierMunozSanchez@users.noreply.github.com>
2020-02-04 12:50:19 +0100

    Cambios menores

--- formreg.php
+++ formreg.php
@@ -3,8 +3,8 @@
  $host_db = "localhost";
  $user_db = "root";
  $pass_db = "";
- $db_name = "basedatosmaster";
- $tbl_name = "Usuarios";
+ $db_name = "munozmurillo";
+ $tbl_name = "usuarios";
  
  $conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
 
@@ -13,31 +13,31 @@
 }
 
  $buscarUsuario = "SELECT * FROM $tbl_name
- WHERE nombre_usuario = '$_POST[username]' ";
+ WHERE nombre_usuario = '$_POST[usuario]' ";
 
  $result = $conexion->query($buscarUsuario);
 
  $count = mysqli_num_rows($result);
 
  if ($count == 1) {
- echo "<br />". "El Nombre de Usuario ya a sido tomado." . "<br />";
+ echo "<br />". "El Nombre de Usuario ya a sido utilizado." . "<br />";
 
- echo "<a href='index.html'>Por favor escoga otro Nombre</a>";
+ echo "<a href='index.php'>Por favor escoga otro Nombre</a>";
  }
  else{
 
- $form_pass = $_POST['password'];
+ $form_pass = $_POST['contrasena'];
  
  $hash = password_hash($form_pass, PASSWORD_BCRYPT);
 
- $query = "INSERT INTO Usuarios (nombre_usuario, password)
-           VALUES ('$_POST[username]', '$hash')";
+ $query = "INSERT INTO Usuarios (nombre, contrasena, email)
+           VALUES ('$_POST[usuario]', '$_POST[contrasena]', '$_POST[email]')";
 
  if ($conexion->query($query) === TRUE) {
  
  echo "<br />" . "<h2>" . "Usuario Creado Exitosamente!" . "</h2>";
- echo "<h4>" . "Bienvenido: " . $_POST['username'] . "</h4>" . "\n\n";
- echo "<h5>" . "Hacer Login: " . "<a href='login.html'>Login</a>" . "</h5>"; 
+ echo "<h4>" . "Bienvenido: " . $_POST['usuario'] . "</h4>" . "\n\n";
+ echo "<h5>" . "Hacer Login: " . "<a href='index.php'>Login</a>" . "</h5>"; 
  }
 
  else {
